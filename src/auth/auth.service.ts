f76c1f162aaf61d0ae08e9010112f26da4f1a0e7
raduubraduu <lefter_radu@yahoo.com>
2023-04-26 16:24:25 +0100

    made prisma module global

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
   
   @Injectable()
   export class AuthService {
+    constructor(private prisma: PrismaService){}
   
      signup() {
     return "hi from signup"
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
+import { PrismaClient } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient { 
+    constructor() {
+    super({
+      datasources: {
+        db: {
+          url: "postgresql://postgres:123@localhost:5434/nest?schema=public",
+        },
+      },
+    });
+  }
+}
